0c88e0363f7624a662f44d295d1d3488b4f34289
Michael Vattuone <mvattuone@gmail.com>
2017-12-09 15:17:35 -0500

    hook up microphone input to audio graph so we can analyse waveform

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-function update(stream) { 
-  console.log(stream + ' is here');
-
-
-  requestAnimationFrame(function () {
-    update(stream);
-  });
-}
 
 function init() { 
+  function isZero(element) { 
+    return element === 0;
+  }
+
+  function update(stream) { 
+    analyserNode.getFloatTimeDomainData(waveform);
+  
+    if (!waveform.every(isZero)) {
+      console.log('there is analyzed sound');
+    } else {
+      console.log('no sound to analyze :'(');
+    }
+
+    requestAnimationFrame(function () {
+      update(stream);
+    });
+  }
+
+  var audioCtx = new AudioContext();
+  var analyserNode = audioCtx.createAnalyser();
+  analyserNode.fftSize = 1024;
+  var waveform = new Float32Array(analyserNode.fftSize);
   window.navigator.mediaDevices.getUserMedia({ audio: true })
     .then(function (stream) { 
-      update(stream);
+      var microphone = audioCtx.createMediaStreamSource(stream);
+      microphone.connect(analyserNode);
+      update(microphone);
     })
     .catch(function (error) {
       throw new Error(error.name);
