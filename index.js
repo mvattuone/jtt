d08bd0ebd2544914a2336239d8cd26004c0a773a
Michael Vattuone <mvattuone@gmail.com>
2017-12-09 14:55:49 -0500

    throw error in getUserMedia catch

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function update(stream) { 
+  console.log(stream + ' is here');
+
+
   requestAnimationFrame(function () {
     update(stream);
   });
-  console.log(stream + ' is here');
 }
 
 function init() { 
   window.navigator.mediaDevices.getUserMedia({ audio: true })
     .then(function (stream) { 
-      console.log('got user media');
       update(stream);
     })
-    .catch(function (err) {
-      console.log('error: ' + err);
+    .catch(function (error) {
+      throw new Error(error.name);
     });
 }
-
 
 init();
